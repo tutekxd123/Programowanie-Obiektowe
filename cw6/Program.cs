75501afe06bbcf5bd32a2c40164703121f38d99d
Piotr Marzec <52503806+tutekxd123@users.noreply.github.com>
2023-12-03 16:20:34 +0100

    Add files via upload

---
+++ cw6/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Car car1 = new ("Volvo",1600);
+        //Car car2 = new("Seat", 2001);
+        var carName = "Mój samochód"; //nie rozumiem sensu
+        Console.WriteLine(carName);
+        car1.wyswietl();
+        //car1 = car2;
+        car1.wyswietl();
+    }
+}
---
+++ cw6/car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Car
+    {
+        private string marka;
+        private int rok;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        public double sredniespalanie;
+        public Car(string marka, int rok)
+        {
+            if(rok<=1769 || rok > 2017)
+            {
+                while(rok<=1769 || rok > 2017)
+                {
+                    Console.WriteLine("Podales zly rok podaj jeszcze raz: ");
+                    rok = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            this.marka = marka;
+            this.rok = rok;
+
+        }
+        public void wyswietl()
+        {
+            Console.WriteLine("Marka: {0} Rok:{1}", this.marka, this.rok);
+        }
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            return (this.sredniespalanie * dlugoscTrasy);
+        }
+        public double ObliczKosztPrzejazdu(double dlugosctrasy,double cenaPaliwa)
+        {
+            return ObliczSpalanie(dlugosctrasy) * cenaPaliwa;
+        }
+    }
+
+}
